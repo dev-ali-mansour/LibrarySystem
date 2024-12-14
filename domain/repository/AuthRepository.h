c97d794371fa3141bd035c83ba8db9fa76809c58
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-14 17:06:45 +0200

    refactor: Create AuthRepository.h Header File As Abstraction Of Authentication Repository class In Domain Layer.

---
+++ domain/repository/AuthRepository.h
@@ -0,0 +1,25 @@
+#ifndef AUTH_REPOSITORY_H
+#define AUTH_REPOSITORY_H
+
+#include<iostream>
+#include "../../data/ds/linked_list/List.cpp"
+#include "../model/User.cpp"
+
+using namespace std;
+
+class AuthRepository {
+    List<long, User> users;
+    long lastId{1};
+
+    void initialize();
+
+public:
+    AuthRepository();
+
+    void signUp(const string &firstName, const string &lastName, const string &email, short age,
+                const string &username, const string &password, bool &result, User &currentUser);
+
+    void signIn(const string &username, const string &password, bool &result, User &user);
+};
+
+#endif //AUTH_REPOSITORY_H
