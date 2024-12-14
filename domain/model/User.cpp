9fee55b46616ed033bc09893b9af152d59d9cec1
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-14 17:04:47 +0200

    refactor: Create User Model Class.

---
+++ domain/model/User.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include "Role.cpp"
+using namespace std;
+
+struct User {
+    long id;
+    string firstName;
+    string lastName;
+    string email;
+    short age;
+    string username;
+    string password;
+    Role role;
+
+    // Default constructor
+    User() = default;
+
+
+    User(
+        const long id,
+        const string &first_name,
+        const string &last_name,
+        const string &email,
+        const short age,
+        const string &username,
+        const string &password,
+        const Role role = Role::USER)
+        : id(id),
+          firstName(first_name),
+          lastName(last_name),
+          email(email),
+          age(age),
+          username(username),
+          password(password),
+          role(role) {
+    }
+};
