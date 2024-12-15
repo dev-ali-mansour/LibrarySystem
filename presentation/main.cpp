ca1936b8e4793c3d156101d8b3215c3bb33a22c3
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-15 15:46:18 +0200

    test: GetBorrowRequestsStatsUseCase, And ProceedBorrowRequestUseCase In Main File.

--- presentation/main.cpp
+++ presentation/main.cpp
@@ -4,12 +4,16 @@
 #include "../domain/use_case/auth/SignUpUseCase.cpp"
 #include "../domain/use_case/book/ListBooksUseCase.cpp"
 #include "../domain/use_case/book/FindBookByTitleUseCase.cpp"
+#include "../domain/use_case/book/GetBorrowRequestsStatsUseCase.cpp"
+#include "../domain/use_case/book/ProceedBorrowRequestUseCase.cpp"
 
 int main() {
     SignInUseCase signInUseCase(AuthRepository::getInstance());
     SignUpUseCase signUpUseCase(AuthRepository::getInstance());
     ListBooksUseCase listBooksUseCase(BookRepository::getInstance());
     FindBookByTitleUseCase findBookByTitleUseCase(BookRepository::getInstance());
+    GetBorrowRequestsStatsUseCase getStatsUseCase(BookRepository::getInstance());
+    ProceedBorrowRequestUseCase proceedUseCase(BookRepository::getInstance());
     string username, password;
     User currentUser;
     bool isValid;
@@ -26,6 +30,16 @@ int main() {
         for (const auto &book: books) {
             cout << "ISBN: " << book.isbn << ", Title: " << book.title << endl;
         }
+
+        int pendingCount, completedCount;
+        getStatsUseCase.execute(pendingCount, completedCount);
+        cout << "Pending Requests Count: " << pendingCount << endl;
+        cout << "Completed Requests Count: " << completedCount << endl;
+        proceedUseCase.execute();
+        getStatsUseCase.execute(pendingCount, completedCount);
+        cout << "Pending Requests Count: " << pendingCount << endl;
+        cout << "Completed Requests Count: " << completedCount << endl;
+
         cout << endl << "Enter Book Title To Search: ";
         string title;
         cin.ignore();
