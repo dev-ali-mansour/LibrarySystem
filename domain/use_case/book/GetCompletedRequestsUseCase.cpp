8da21db4aa916a145a330be1435fecc0a6754ae8
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-15 15:45:39 +0200

    feat: Create GetCompletedRequestsUseCase.cpp As Getting Completed Borrow Requests Use Case In Domain Layer.

---
+++ domain/use_case/book/GetCompletedRequestsUseCase.cpp
@@ -0,0 +1,13 @@
+#include "../../repository/BookRepository.h"
+
+class GetCompletedRequestsUseCase {
+    BookRepository *repo;
+
+public:
+    explicit GetCompletedRequestsUseCase(BookRepository *repository) : repo(repository) {
+    }
+
+    vector<BorrowRequest> execute() {
+        return repo->getCompleteedRequests();
+    }
+};
