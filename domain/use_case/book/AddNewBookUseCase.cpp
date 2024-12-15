5ec032472f91b754ab9c0f56697e0ec6544b725c
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-15 05:08:14 +0200

    feat: Create AddNewBookUseCase.cpp As Adding New Book Use Case In Domain Layer.

---
+++ domain/use_case/book/AddNewBookUseCase.cpp
@@ -0,0 +1,19 @@
+#include "../../repository/BookRepository.h"
+
+class AddNewBookUseCase {
+    BookRepository repo;
+
+public:
+    explicit AddNewBookUseCase(BookRepository &repository) : repo(repository) {
+    }
+
+    bool execute(
+            const string &isbn,
+            const string &title,
+            const string &author,
+            const short &version,
+            const int &publishingYear,
+            const int &pages) {
+        return repo.addNewBook(isbn, title, author, version, pages);
+    }
+};
