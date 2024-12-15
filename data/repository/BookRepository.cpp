f81e586fa055745c32130e950e187f671586a95e
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-15 09:09:59 +0200

    refactor: Implement BookRepository::undo() In BookRepository.cpp.

--- data/repository/BookRepository.cpp
+++ data/repository/BookRepository.cpp
@@ -31,6 +31,31 @@ bool BookRepository::removeBook(const string &isbn) {
     return false;
 }
 
+void BookRepository::undo() {
+    if (actions.stackIsEmpty()) {
+        cerr << "There is no more actions to undo!\n";
+        return;
+    }
+    BookAction action;
+    actions.pop(action);
+    bool result = false;
+    if (action.type == ActionType::ADD) {
+        result = removeBook(action.book.isbn);
+        cout << "Removing The book " << action.book.title << endl;
+    } else if (action.type == ActionType::REMOVE) {
+        result = addNewBook(
+                action.book.isbn,
+                action.book.title,
+                action.book.author,
+                action.book.version,
+                action.book.publishingYear,
+                action.book.pages);
+        cout << "Re-adding The book " << action.book.title << endl;
+    }
+    if (result)cout << "Operation done successfully!\n";
+    else cerr << "\nFailed to do operation!\n";
+}
+
 vector<Book> BookRepository::findBookByIsbn(const string &isbn) {
     vector<Book> list;
     //Todo(Not Implemented Yet)
