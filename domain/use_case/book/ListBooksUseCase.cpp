f2cc677a84adf33122a67525e703397f62c29c1a
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-15 05:02:06 +0200

    feat: Create ListBooksUseCase.cpp As List All Books Use Case In Domain Layer.

---
+++ domain/use_case/book/ListBooksUseCase.cpp
@@ -0,0 +1,13 @@
+#include "../../repository/BookRepository.h"
+
+class ListBooksUseCase {
+    BookRepository repo;
+
+public:
+    explicit ListBooksUseCase(BookRepository &repository) : repo(repository) {
+    }
+
+    vector<Book> execute() {
+        return repo.listBooks();
+    }
+};
