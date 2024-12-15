0c55d3a232d1ca02e1a53e36a880011caa6d8a0c
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-15 04:59:10 +0200

    refactor: Add pages Data Member To Book Model Class.

--- domain/model/Book.cpp
+++ domain/model/Book.cpp
@@ -8,16 +8,21 @@ struct Book {
     string author;
     short version;
     int publishingYear;
+    int pages;
+
+    Book() = default;
 
     Book(const string &isbn,
          const string &title,
          const string &author,
          const short version,
-         const int publishing_year)
-        : isbn(isbn),
-          title(title),
-          author(author),
-          version(version),
-          publishingYear(publishing_year) {
+         const int publishing_year,
+         int pages
+    ) : isbn(isbn),
+        title(title),
+        author(author),
+        version(version),
+        publishingYear(publishing_year),
+        pages(pages) {
     }
 };
