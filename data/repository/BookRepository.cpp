87bfaeb4194a4a7a0c9b8cc1ebd9576d4baadd2a
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-15 15:42:56 +0200

    refactor: Implement New Created Functions Of BookRepository.cpp.

--- data/repository/BookRepository.cpp
+++ data/repository/BookRepository.cpp
@@ -1,6 +1,10 @@
 #include "../../domain/repository/BookRepository.h"
+#include "../../data/util/Utilities.cpp"
+#include <chrono>
 
-BookRepository* BookRepository::instance = nullptr;
+using namespace chrono;
+
+BookRepository *BookRepository::instance = nullptr;
 
 void BookRepository::initialize() {
     const Book book1("9780134383583", "Discovering Modern C++", "Peter Gottschling", 1, 2015, 480);
@@ -9,6 +13,12 @@ void BookRepository::initialize() {
     books.insertFirst(book1.isbn, book1);
     books.orderInsert(book2.isbn, book2);
     books.orderInsert(book3.isbn, book3);
+    const BorrowRequest request("9780134383583", 2, Utilities::getTime());
+    pendingRequests.enqueue(request);
+    const BorrowRequest request2("9781507707616", 2, Utilities::getTime());
+    pendingRequests.enqueue(request2);
+    const BorrowRequest request3("9781517080402", 2, Utilities::getTime());
+    pendingRequests.enqueue(request3);
 }
 
 BookRepository::BookRepository() {
@@ -16,18 +26,29 @@ BookRepository::BookRepository() {
 }
 
 bool BookRepository::addNewBook(
-        const string &isbn,
-        const string &title,
-        const string &author,
-        const short &version,
-        const int &publishingYear,
-        const int &pages) {
-    //Todo(Not Implemented Yet)
-    return false;
+    const string &isbn,
+    const string &title,
+    const string &author,
+    const short &version,
+    const int &publishingYear,
+    const int &pages) {
+    const Book book1(isbn, title, author, version,publishingYear,pages );
+    books.orderInsert(book1.isbn,book1);
+    return true;
 }
 
 bool BookRepository::removeBook(const string &isbn) {
-    //Todo(Not Implemented Yet)
+    books.toFirst();
+    Book book;
+
+    while (!books.cursorIsEmpty()) {
+    books.retrieveData(book);
+        if (book.isbn==isbn) {
+            books.deleteNode();
+            return true;
+        }
+        books.advance();
+    }
     return false;
 }
 
@@ -44,12 +65,12 @@ void BookRepository::undo() {
         cout << "Removing The book " << action.book.title << endl;
     } else if (action.type == ActionType::REMOVE) {
         result = addNewBook(
-                action.book.isbn,
-                action.book.title,
-                action.book.author,
-                action.book.version,
-                action.book.publishingYear,
-                action.book.pages);
+            action.book.isbn,
+            action.book.title,
+            action.book.author,
+            action.book.version,
+            action.book.publishingYear,
+            action.book.pages);
         cout << "Re-adding The book " << action.book.title << endl;
     }
     if (result)cout << "Operation done successfully!\n";
@@ -73,7 +94,7 @@ vector<Book> BookRepository::findBookByTitle(const string &title) {
     while (!books.cursorIsEmpty()) {
         books.retrieveData(book);
         // Check if book title contains the title parameter
-        if (StringSearch::caseInsensitiveSubstringSearch(book.title, title))
+        if (Utilities::caseInsensitiveSubstringSearch(book.title, title))
             list.push_back(book);
         books.advance();
     }
@@ -100,5 +121,32 @@ vector<Book> BookRepository::listBooks() {
         list.push_back(book);
         books.advance();
     }
+    return list;
+}
+
+void BookRepository::requestToBorrowBook(
+    const string &isbn,
+    const long &user_id,
+    const string &created) {
+    //Todo(Not Implemented Yet)
+}
+
+void BookRepository::proceedBorrowRequest() {
+    //Todo(Not Implemented Yet)
+}
+
+void BookRepository::getBorrowRequestsStats(int &pendingCount, int &completedCount) const {
+    //Todo(Not Implemented Yet)
+}
+
+vector<BorrowRequest> BookRepository::getPendingRequests() {
+    vector<BorrowRequest> list;
+    //Todo(Not Implemented Yet)
+    return list;
+}
+
+vector<BorrowRequest> BookRepository::getCompletedRequests() {
+    vector<BorrowRequest> list;
+    //Todo(Not Implemented Yet)
     return list;
 }
