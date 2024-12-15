93690b45f414fbf8621ebd53f08264f7c019a7df
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-15 06:11:17 +0200

    feat: Create FindBookByTitleUseCase.cpp As Finding Book By Its Title Use Case In Domain Layer.

---
+++ domain/use_case/book/FindBookByTitleUseCase.cpp
@@ -0,0 +1,13 @@
+#include "../../repository/BookRepository.h"
+
+class FindBookByTitleUseCase {
+    BookRepository repo;
+
+public:
+    explicit FindBookByTitleUseCase(BookRepository &repository) : repo(repository) {
+    }
+
+    vector<Book> execute(const string &title) {
+        return repo.findBookByTitle(title);
+    }
+};
