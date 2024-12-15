276b7d5be582b1331db07d84c4e116fc810747a4
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-15 04:58:19 +0200

    refactor: Create BookRepository.cpp As Implementation Of Book Repository Class In Data Layer.

---
+++ data/repository/BookRepository.cpp
@@ -0,0 +1,53 @@
+
+#include "../../domain/repository/BookRepository.h"
+
+void BookRepository::initialize() {
+    const Book book1("9780134383583", "Discovering Modern C++", "Peter Gottschling", 1, 2015, 480);
+    const Book book2("9781507707616", "C# Programming for Beginners", "Troy Dimes", 1, 2015, 106);
+    const Book book3("9781517080402", "JAVA Programming for Beginners", "Tim Warren", 1, 2002, 68);
+    books.insertFirst(book1.isbn, book1);
+    books.orderInsert(book2.isbn, book2);
+    books.orderInsert(book3.isbn, book3);
+}
+
+BookRepository::BookRepository() {
+    initialize();
+}
+
+bool BookRepository::addNewBook(
+        const string &isbn,
+        const string &title,
+        const string &author,
+        const short &version,
+        const int &publishingYear,
+        const int &pages) {
+    //Todo(Not Implemented Yet)
+}
+
+bool BookRepository::removeBook(const string &isbn) {
+    //Todo(Not Implemented Yet)
+}
+
+vector<Book> BookRepository::findBookByIsbn(const string &isbn) {
+    //Todo(Not Implemented Yet)
+}
+
+vector<Book> BookRepository::findBookByAuthor(const string &author) {
+    //Todo(Not Implemented Yet)
+}
+
+vector<Book> BookRepository::listBooks() {
+    vector<Book> list;
+    if (books.listIsEmpty()) {
+        cerr << "No books found!\n";
+        return list;
+    }
+    Book book;
+    books.toFirst();
+    while (!books.cursorIsEmpty()) {
+        books.retrieveData(book);
+        list.push_back(book);
+        books.advance();
+    }
+    return list;
+}
