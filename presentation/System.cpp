6b2f48b0eba226583b702456dd82ff08a1d5c1d8
Ali Mansour <dev.ali.mansour@gmail.com>
2024-12-16 08:21:35 +0200

    refactor: Rename main.cpp To System.cpp And Clean It.
    Show auth menu in the startup of the system.

---
+++ presentation/System.cpp
@@ -0,0 +1,10 @@
+#include "main/MainScreen.h"
+
+int main() {
+    cout << "######################\n";
+    cout << "Library System\n";
+    cout << "######################\n";
+    showAuthMenu();
+
+    return 0;
+}
